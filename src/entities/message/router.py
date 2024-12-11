1aac5c3c78c48362594f850d11d3d7ea0467a7d5
userofgh <test>
2024-12-11 21:55:02 +0200

    ruff linter changes

--- src/entities/message/router.py
+++ src/entities/message/router.py
@@ -1,7 +1,6 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, Query, status
-from fastapi_cache.decorator import cache
 
 from src.entities.auth.dependencies import get_current_user
 from src.entities.message.dependencies import MessageServiceObj
